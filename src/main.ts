725d6fb06606fa8cd04f60becd4b08ad40eee81b
Erick Solano <30907865+Easd99@users.noreply.github.com>
2025-06-03 11:25:07 -0500

    fix: fix

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { initializeTransactionalContext } from 'typeorm-transactional';
 import * as process from 'node:process';
+import { json } from 'body-parser';
 
 async function bootstrap() {
   initializeTransactionalContext();
   const app = await NestFactory.create(AppModule);
+
+  app.use(json());
+
+  app.enableCors({
+    origin: '*',
+    methods: ['POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
 
   app.setGlobalPrefix('api/v1');
 
